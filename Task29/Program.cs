35ecc4ff65ead5903b29541d629cf4fddb09fe3b
Pavel <dispitcher@mail.ru>
2022-10-24 20:07:47 +0300

    End Task

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,20 +1,19 @@
 ﻿/* Напишите программу, которая задаёт массив произвольной длины, заполняет произвольными элементами и выводит их на экран.
 Длину массива и элементы массива можно задать рандомно или попросить пользователя ввести в консоли.*/
-int[] arr = new int[Length];
 
-void newArray(int[]array)
+
+void newArray(int[]array, int min, int max)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(min, max);
+        array[i] = new Random().Next(min, max + 1);
     }
 }
 void PrintArray(int[] array)
 {
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]}");
         if (i < (array.Length-1)) Console.Write(", ");
     }
 }
@@ -24,5 +23,8 @@ Console.Write("Введите min ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите max ");
 int max = Convert.ToInt32(Console.ReadLine());
-newArray(arr);
+
+int[] arr = new int[length];
+
+newArray(arr, min, max);
 PrintArray(arr);
