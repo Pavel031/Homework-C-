c07e7a8b5a00aef01b67512457e7e08ff903afb0
Pavel <dispitcher@mail.ru>
2022-11-09 18:50:23 +0300

    Среднее арифметическое столбцов

---
+++ Task52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = new Int32[3, 4];
+
+fillArray(array, 0, 10);
+printArray(array);
+arithmeticMean(array);
+
+void fillArray(int[,] arr, int min, int max)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "  ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void arithmeticMean(int[,] arr)
+{
+    int sum = 0;
+    Console.Write("Среднее арифметическое каждого столбца : ");
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        Console.Write(Math.Round((double)sum / arr.GetLength(0), 1) + "  ");
+        sum = 0;
+    }
+}
+
+
