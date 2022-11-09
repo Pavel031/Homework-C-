fcd306fe9fd8b40b71b6b9819cbc88c6e587b775
Pavel <dispitcher@mail.ru>
2022-11-09 18:38:09 +0300

    Двумерный массив

---
+++ Task47/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] array = new Double[3, 4];
+
+fillArray(array);
+printArray(array);
+
+void fillArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round((new Random().NextDouble() * 100), 1);
+        }
+    }
+}
+
+
+void printArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "  ");
+        }
+
+        Console.WriteLine();
+    }
+}
