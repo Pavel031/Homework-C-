c9d1a30249ff641927b38aefd89d235eb8a371b2
Pavel <dispitcher@mail.ru>
2022-11-17 14:43:59 +0300

    Рекурсия сумма элементов

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите первое число: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите последнее число: ");
+int finish = Convert.ToInt32(Console.ReadLine());
+
+Numbers(start, finish, 0);
+
+
+void Numbers(int n, int m, int sum) {
+
+    if (n > m) 
+    {
+        Console.WriteLine($"Сумма элементов равна: {sum}"); 
+        return;
+    }
+    sum += (n ++);
+    Numbers(n, m, sum);
+}
