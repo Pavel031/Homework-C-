b2d32091c1e459df5b775af8e3bb03fbf3fba048
Pavel <dispitcher@mail.ru>
2022-11-17 14:04:45 +0300

    Рекурсия натуральные числа

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+printNaturalNumbers(number);
+
+
+void printNaturalNumbers(int n) {
+
+    if(n == 1 ) {
+        Console.Write(n);
+    } else {
+        Console.Write(n + " ");
+        printNaturalNumbers(n-1);
+    }
+}
